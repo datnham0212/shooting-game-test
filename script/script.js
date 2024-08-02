733418b09d9d4eb52522e218b78159c5021aa57f
datnham0212 <90122319+datnham0212@users.noreply.github.com>
2024-08-02 10:05:07 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
-function playGunShot(){
-    var gunshot = new Audio('sfx/laser.mp3');
-    gunshot.play();
-}
-document.addEventListener("click", playGunShot);
+$(document).ready(function() {
+
+    function playGunShot(){
+        var gunshot = new Audio('sfx/laser.mp3');
+        gunshot.play();
+    }
+    $(document).click(playGunShot);
+
+    function bouncing(){
+        var maxWidth = $(window).width() - $('.target').width();
+        var maxHeight = $(window).height() - $('.target').height();
+
+        var newLeft = maxWidth * Math.random();
+        var newTop = maxHeight * Math.random();
+
+        $('.target').animate({left: newLeft, top: newTop}, 1500, function() {bouncing();}
+        );
+    }
+
+    bouncing();
+});
